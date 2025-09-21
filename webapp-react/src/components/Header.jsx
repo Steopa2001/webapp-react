df4e1893b95f57e70da1990cd27e4081363e57e4
Steopa2001 <moraru495@gmail.com>
2025-09-21 16:48:19 +0200

    feat: create the header style

--- webapp-react/src/components/Header.jsx
+++ webapp-react/src/components/Header.jsx
@@ -4,8 +4,9 @@ const Header = () => {
   return (
     <header className="container-fluid">
       <div className="row">
-        <div className="col-12">
-            <h1>BoolMovies</h1>
+        <div className="col-12 d-flex align-items-center">
+            <img className='logo-movies' src="../../public/img/logo-Movie.png" alt="logo" />
+            <h2 className={`${style.title_style}`}>MY MOVIES</h2>
         </div>
       </div>
     </header>
--- webapp-react/src/pages/Home.jsx
+++ webapp-react/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1>Home</h1>
+        </div>
+        <div className="col-12"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
