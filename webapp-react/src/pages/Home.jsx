4a33e065073361a8f0404751c31814af1da651ea
Steopa2001 <moraru495@gmail.com>
2025-09-21 18:13:36 +0200

    feat: fetch and display movies list on home page

--- webapp-react/src/pages/Home.jsx
+++ webapp-react/src/pages/Home.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import axios from 'axios';
+import { useState, useEffect } from "react";
 
 const Home = () => {
+
+    //definisco variabili di stato
+    const [movies, setMovies] = useState([]);
+
+    //funzione che recupera i film attraverso la chiamata ajax
+    const fetchMovies = () => {
+        axios.get('http://localhost:3000/api/movies').then((resp) => {
+            setMovies(resp.data);
+            console.log(resp.data)
+        }).catch((err) => console.log(err));
+    };
+
+    //utilizzo useEffect per recuperare la lista dei film
+    useEffect(fetchMovies, []);
+
   return (
     <div className="container my-5">
       <div className="row">
@@ -9,15 +26,20 @@ const Home = () => {
         </div>
         </div>
         <div className="row">
-        <div className="col-12 col-md-6 col-lg-4">
+            {movies.map((movie) => {
+                return (
+        <div className="col-12 col-md-6 col-lg-4" key={movie.id}>
             <div className="card-movie">
-                <img src="../../public/img/inception.jpg" className="cover-movie-card" alt="film" />
+                <img src={`/img/${movie.image}`} className="cover-movie-card" alt={movie.title} />
                 <div className="overlay">
-                    <h2>Titolo movies</h2>
-                    <p>Autore</p>
+                    <h2>{movie.title}</h2>
+                    <p><em>{movie.director}</em></p>
+                    <p>{movie.genre}</p>
                 </div>
             </div>
         </div>
+                )
+            })}
         </div>
       </div>
   );
