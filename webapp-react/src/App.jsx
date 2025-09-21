d24ac630fd73e5d1633b6802b7372290dcb1c1d8
Steopa2001 <moraru495@gmail.com>
2025-09-21 14:26:08 +0200

    feat: configure app routing with layout, home and movie detail pages

--- webapp-react/src/App.jsx
+++ webapp-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
@@ -7,9 +7,12 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Router>
-          <Route />
-        </Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies/:id" element={<MovieDetail />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
