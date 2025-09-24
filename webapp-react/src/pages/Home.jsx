1b59bc0511185c522d72ad8fd3c6f074634174e4
Steopa2001 <moraru495@gmail.com>
2025-09-24 18:44:05 +0200

    feat: add overlay loader with centered large spinner

--- webapp-react/src/pages/Home.jsx
+++ webapp-react/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
 import { Link } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const Home = () => {
   //definisco variabili di stato
@@ -23,9 +24,10 @@ const Home = () => {
 
   return (
     <div className="container my-5">
+      <Loader/>
       <div className="row mb-4">
         <div className="col-12 text-center lista-libri">
-          <h1>Lista Libri</h1>
+          <h1>Lista Film</h1>
         </div>
         <div className="col-12 text-end">
           <Link to="/movies/create" className="btn btn-primary">
